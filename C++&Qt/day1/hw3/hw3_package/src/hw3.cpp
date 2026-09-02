aa66fca90d28946519a548294a96016dd16972e2
j00h0jin <hojin7710@daum.net>
2026-09-03 01:53:49 +0900

    🚚 chore: add seed file

---
+++ C++&Qt/day1/hw3/hw3_package/src/hw3.cpp
@@ -0,0 +1,21 @@
+#include "../include/hw3.hpp"
+
+int Monster::Be_Attacked()
+{
+}
+
+void Player::Attack(Monster &target)
+{
+}
+
+void Player::Show_status()
+{
+}
+
+void Player::X_move(int move)
+{
+}
+
+void Player::Y_move(int move)
+{
+}
---
+++ C++&Qt/day1/hw3/hw3_package/src/hw3_main.cpp
@@ -0,0 +1,11 @@
+#include "../include/hw3.hpp"
+#include <iostream>
+
+int main()
+{
+    Player player(0, 0);
+    Monster monster(5, 4, 50);
+
+    std::cout << "test" << std::endl;
+    return 0;
+}
