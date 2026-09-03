895408dce1ed026d6fefc28a742b6dd45ce51dca
j00h0jin <hojin7710@daum.net>
2026-09-03 18:55:39 +0900

    ✅ feat: add cQt day1 hw3

--- C++&Qt/day1/hw3/hw3_package/src/hw3.cpp
+++ C++&Qt/day1/hw3/hw3_package/src/hw3.cpp
@@ -1,21 +1,63 @@
 #include "../include/hw3.hpp"
+#include <iostream>
+
+using namespace std;
+
+Monster::Monster(int x, int y, int HP)
+{
+    this->x = x;
+    this->y = y;
+    this->HP = HP;
+}
+Player::Player(int x, int y)
+{
+    this->x = x;
+    this->y = y;
+}
 
 int Monster::Be_Attacked()
 {
+    HP = HP - 10;
+    cout << "남은 체력: " << HP << endl;
+
+    if (HP <= 0) // 1이면 사망
+        return 1;
+    else
+        return 0;
 }
 
 void Player::Attack(Monster &target)
 {
+
+    // 좌표 맞고 MP 있을 때, MP 부족일 때, 그 외로 분류
+    if (target.x == x && target.y == y && MP > 0)
+    {
+        MP = MP - 1;
+        cout << "공격 성공!" << endl;
+        target.Be_Attacked();
+    }
+    else
+    {
+        MP = MP - 1;
+        cout << "공격 실패!" << endl;
+    }
 }
 
 void Player::Show_status()
 {
+    cout << "HP: " << HP << endl;
+    cout << "MP: " << MP << endl;
+    cout << "Position: " << x << ", " << y << endl;
 }
 
 void Player::X_move(int move)
 {
+    x = x + move;
+    cout << "X Position " << move << " moved!" << endl;
 }
 
 void Player::Y_move(int move)
 {
+    y = y + move;
+    cout << "Y Position " << move << " moved!" << endl;
 }
--- C++&Qt/day1/hw3/hw3_package/src/hw3_main.cpp
+++ C++&Qt/day1/hw3/hw3_package/src/hw3_main.cpp
@@ -1,11 +1,81 @@
+/*
+A U D R L S
+
+A: 어택
+U: y +1
+D: y -1
+R: x +1
+L: x -1
+S: status(현재 hp, mp, 좌표) player 기준 50 10이 디폴트인 것 같음
+
+monster와 좌표가 동일할 때 공격이 성공하는 것 같음
+다르면 실패(MP 차감)
+공격력은 10
+*/
+
 #include "../include/hw3.hpp"
 #include <iostream>
 
 int main()
 {
     Player player(0, 0);
-    Monster monster(5, 4, 50);
+    Monster monster(1, 1, 50);
 
-    std::cout << "test" << std::endl;
+    player.HP = 50, player.MP = 10;
+
+    char ch;
+    int is_end = 0;
+
+    while (1)
+    {
+        std::cout << "Type Command(A/U/D/R/L/S)" << std::endl;
+        std::cin >> ch;
+
+        switch (ch)
+        {
+        case 65: // A
+            if (player.MP == 0)
+            {
+                is_end = 1;
+                break;
+            }
+            player.Attack(monster);
+            if (monster.HP == 0)
+            {
+                is_end = 1;
+                break;
+            }
+            break;
+
+        case 85: // U
+            player.Y_move(1);
+            break;
+
+        case 68: // D
+            player.Y_move(-1);
+            break;
+
+        case 82: // R
+            player.X_move(1);
+            break;
+
+        case 76: // L
+            player.X_move(-1);
+            break;
+
+        case 83: // S
+            player.Show_status();
+            break;
+
+        default:
+            break;
+        }
+
+        if (is_end == 1)
+            break;
+
+        std::cout << std::endl;
+    }
+
     return 0;
 }
