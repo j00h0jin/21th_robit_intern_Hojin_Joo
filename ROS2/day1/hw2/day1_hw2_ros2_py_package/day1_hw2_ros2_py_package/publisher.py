beb53d727a75e75f6c46df2fb6245f6a9bd61467
j00h0jin <hojin7710@daum.net>
2026-09-09 04:24:56 +0900

    ✅ feat: ROS2 day1 hw2 topic py

--- ROS2/day1/hw2/day1_hw2_ros2_py_package/day1_hw2_ros2_py_package/publisher.py
+++ ROS2/day1/hw2/day1_hw2_ros2_py_package/day1_hw2_ros2_py_package/publisher.py
@@ -15,12 +15,16 @@ class HelloworldPublisher(Node):
   # 클래스 생성자 정의
   # 부모 클래스(Node)의 생성자를 호출하고 노드 이름을 helloworld_publisher로 지정
   def __init__(self):
-    super().__init__('helloworld_publisher')
+    super().__init__('py_publisher')
     qos_profile = QoSProfile(depth=10)
     
-    # Nod 클래스의 create_publisher 함수를 이용해 helloworld_publisher를 설정함
+    # Node 클래스의 create_publisher 함수를 이용해 helloworld_publisher를 설정함
     # 매개변수로 토픽 메시지 타입은 String, 토픽 이름은 helloworld, QoS는 qos_prpfile
-    self.helloworld_publisher = self.create_publisher(String, 'helloworld', qos_profile)
+    self.string_publisher = self.create_publisher(String, 'string', qos_profile)
+    self.int_publisher = self.create_publisher(Int32, 'int', qos_profile)
+    self.bool_publisher = self.create_publisher(Bool, 'bool', qos_profile)
+    self.float_publisher = self.create_publisher(Float32, 'float', qos_profile)
+    
     self.timer = self.create_timer(1, self.publish_helloworld_msg)
     self.count = 0
     
@@ -28,10 +32,26 @@ class HelloworldPublisher(Node):
   # 실제 데이터는 msg.data에 저장
   def publish_helloworld_msg(self):
     msg = String()
-    msg.data = 'Hello World: {0}'.format(self.count)
-    self.helloworld_publisher.publish(msg)
-    self.get_logger().info('Published message: {0}'.format(msg.data))
+    msg.data = f'Hello World {self.count}'
+    self.string_publisher.publish(msg)
+    self.get_logger().info(f'Published message: {msg.data}')
     self.count += 1
+    
+    msgInt = Int32();
+    msgInt.data = 2024405020
+    self.int_publisher.publish(msgInt)
+    self.get_logger().info(f'Published message: {msgInt.data}')
+    
+    msgBool = Bool()
+    msgBool.data = False
+    self.bool_publisher.publish(msgBool)
+    self.get_logger().info(f'Published message: {msgBool.data}')
+    
+    msgFloat = Float32()
+    msgFloat.data = 3.141592
+    self.float_publisher.publish(msgFloat)
+    self.get_logger().info(f'published message: {msgFloat.data}')
+  
 
 
 # rclpy.init을 이용해 초기화
--- ROS2/day1/hw2/day1_hw2_ros2_py_package/day1_hw2_ros2_py_package/subscriber.py
+++ ROS2/day1/hw2/day1_hw2_ros2_py_package/day1_hw2_ros2_py_package/subscriber.py
@@ -9,12 +9,15 @@ from std_msgs.msg import Float32
 class HelloworldSubscriber(Node):
 
   def __init__(self):
-    super().__init__('helloworld_subscriber')
+    super().__init__('py_subscriber')
     qos_profile = QoSProfile(depth=10)
-    self.helloworld_subscriber = self.create_subscription(String, 'helloworld', self.subscribe_topic_message, qos_profile)
-
+    self.helloworld_subscriber = self.create_subscription(String, 'string', self.subscribe_topic_message, qos_profile)
+    self.int_subscriber = self.create_subscription(Int32, 'int', self.subscribe_topic_message, qos_profile)
+    self.bool_subscriber = self.create_subscription(Bool, 'bool', self.subscribe_topic_message, qos_profile)
+    self.float_subscriber = self.create_subscription(Float32, 'float', self.subscribe_topic_message, qos_profile)
+    
   def subscribe_topic_message(self, msg):
-    self.get_logger().info('Reveived message: {0}'.format(msg.data))
+    self.get_logger().info(f'Reveived message: {msg.data}')
 
 
 def main(args=None):
