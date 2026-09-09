6317e580f324a8c45771018a66af9a4b3f2a5315
j00h0jin <hojin7710@daum.net>
2026-09-09 22:59:19 +0900

    🌱 chore: add seed file

---
+++ ROS2/day2/hw2/day2_hw2_package/include/day2_hw2_package/main_window.hpp
@@ -0,0 +1,41 @@
+/**
+ * @file /include/day2_hw2_package/main_window.hpp
+ *
+ * @brief Qt based gui for %(package)s.
+ *
+ * @date August 2024
+ **/
+
+#ifndef day2_hw2_package_MAIN_WINDOW_H
+#define day2_hw2_package_MAIN_WINDOW_H
+
+/*****************************************************************************
+** Includes
+*****************************************************************************/
+
+#include <QMainWindow>
+#include "QIcon"
+#include "qnode.hpp"
+#include "ui_mainwindow.h"
+
+/*****************************************************************************
+** Interface [MainWindow]
+*****************************************************************************/
+/**
+ * @brief Qt central, all operations relating to the view part here.
+ */
+class MainWindow : public QMainWindow
+{
+  Q_OBJECT
+
+public:
+  MainWindow(QWidget* parent = nullptr);
+  ~MainWindow();
+  QNode* qnode;
+
+private:
+  Ui::MainWindow* ui;
+  void closeEvent(QCloseEvent* event);
+};
+
+#endif  // day2_hw2_package_MAIN_WINDOW_H
