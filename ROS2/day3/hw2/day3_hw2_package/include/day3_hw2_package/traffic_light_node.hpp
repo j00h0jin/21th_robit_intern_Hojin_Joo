f2a7eae2f94c5138a7c8a7eec20c093a5c588f7f
j00h0jin <hojin7710@daum.net>
2026-09-11 07:57:42 +0900

    🚧 feat: add traffic light node

--- ROS2/day3/hw2/day3_hw2_package/include/day3_hw2_package/traffic_light_node.hpp
+++ ROS2/day3/hw2/day3_hw2_package/include/day3_hw2_package/traffic_light_node.hpp
@@ -4,6 +4,7 @@
 ** Includes
 *****************************************************************************/
 #include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
 
 /*****************************************************************************
 ** Class
@@ -12,4 +13,16 @@ class TrafficLightNode : public rclcpp::Node
 {
   public:
     TrafficLightNode();
+
+  private:
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr state_publisher_;
+    rclcpp::TimerBase::SharedPtr timer_;
+
+    std::string current_state_;
+    double red_duration_;
+    double green_duration_;
+    double yellow_duration_;
+    double elapsed_time_{0.0};
+
+    void timer_callback();
 };
--- ROS2/day3/hw2/day3_hw2_package/src/traffic_light_node.cpp
+++ ROS2/day3/hw2/day3_hw2_package/src/traffic_light_node.cpp
@@ -12,8 +12,53 @@
 
 #include "../include/day3_hw2_package/traffic_light_node.hpp"
 
+#include <chrono>
+
 TrafficLightNode::TrafficLightNode() : Node("traffic_light_node")
 {
+    state_publisher_ = create_publisher<std_msgs::msg::String>("traffic_light_state", 10);
+
+    declare_parameter<double>("red_duration", 5.0);
+    declare_parameter<double>("green_duration", 5.0);
+    declare_parameter<double>("yellow_duration", 2.0);
+
+    red_duration_ = get_parameter("red_duration").as_double();
+    green_duration_ = get_parameter("green_duration").as_double();
+    yellow_duration_ = get_parameter("yellow_duration").as_double();
+
+    current_state_ = "GREEN";
+    timer_ = create_wall_timer(std::chrono::milliseconds(100), std::bind(&TrafficLightNode::timer_callback, this));
+}
+
+void TrafficLightNode::timer_callback()
+{
+    const double timer_period = 0.1; // create_wall_timer(milliseconds(100), ...
+    elapsed_time_ += timer_period;
+
+    double state_duration = 0.0;
+
+    if (current_state_ == "RED")
+        state_duration = red_duration_;
+    else if (current_state_ == "GREEN")
+        state_duration = green_duration_;
+    else
+        state_duration = yellow_duration_;
+
+    if (elapsed_time_ >= state_duration) // 해당 light duration이 지나면 다음 색으로 변경
+    {
+        // red -> green -> yellow -> red
+        elapsed_time_ = 0.0;
+        if (current_state_ == "RED")
+            current_state_ = "GREEN";
+        else if (current_state_ == "GREEN")
+            current_state_ = "YELLOW";
+        else
+            current_state_ = "RED";
+    }
+
+    std_msgs::msg::String message;
+    message.data = current_state_;
+    state_publisher_->publish(message);
 }
 
 int main(int argc, char *argv[])
