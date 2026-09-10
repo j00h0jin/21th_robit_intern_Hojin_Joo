cee98cfe3fab366f3db50a9f5fe3f1ae6702d621
j00h0jin <hojin7710@daum.net>
2026-09-11 07:57:22 +0900

    🚧 feat: change yaml file parameter

--- ROS2/day3/hw2/day3_hw2_package/launch/day3_hw2_launch.py
+++ ROS2/day3/hw2/day3_hw2_package/launch/day3_hw2_launch.py
@@ -6,10 +6,15 @@ from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
     # 패키지 내 config/test.yaml 경로를 상대 참조로 생성
-    yaml_file_path = PathJoinSubstitution([
+    traffic_light_yaml_file_path = PathJoinSubstitution([
         FindPackageShare('day3_hw2_package'),
         'config',
-        'temp.yaml'
+        'traffic_light.yaml'
+    ])
+    vehicle_yaml_file_path = PathJoinSubstitution([
+        FindPackageShare('day3_hw2_package'),
+        'config',
+        'vehicle.yaml'
     ])
 
     return LaunchDescription([
@@ -17,13 +22,13 @@ def generate_launch_description():
             package='day3_hw2_package',
             executable='traffic_light_node',
             name='traffic_light_node',
-            parameters=[yaml_file_path]
+            parameters=[traffic_light_yaml_file_path]
         ),
         Node(
             package='day3_hw2_package',
             executable='vehicle_node',
             name='vehicle_node',
-            parameters=[yaml_file_path]
+            parameters=[vehicle_yaml_file_path]
         ),
         Node(
             package='day3_hw2_package',
