e22ead9e1a450e1dd883303b63f92db1a096d880
j00h0jin <hojin7710@daum.net>
2026-09-11 17:08:09 +0900

    🚧 chore: edit day hw2 sub node

--- ROS2/day3/hw2/day3_hw2_package/include/day3_hw2_package/day3_hw2_sub_node.hpp
+++ ROS2/day3/hw2/day3_hw2_package/include/day3_hw2_package/day3_hw2_sub_node.hpp
@@ -3,6 +3,7 @@
 #include "rclcpp/rclcpp.hpp"
 
 #include "std_msgs/msg/string.hpp"
+#include <geometry_msgs/msg/pose2_d.hpp>
 
 class Day3Hw2SubNode : public rclcpp::Node
 {
@@ -14,9 +15,19 @@ class Day3Hw2SubNode : public rclcpp::Node
         return traffic_state_;
     }
 
+    geometry_msgs::msg::Pose2D vehicle_position() const
+    {
+      return cur_coordinate_;
+    }
+
   private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg);
+    void traffic_topic_callback(const std_msgs::msg::String::SharedPtr msg);
 
     std::string traffic_state_;
     rclcpp::Subscription<std_msgs::msg::String>::SharedPtr sub_traffic;
+
+    void vehicle_topic_callback(const geometry_msgs::msg::Pose2D::SharedPtr msg);
+
+    geometry_msgs::msg::Pose2D cur_coordinate_;
+    rclcpp::Subscription<geometry_msgs::msg::Pose2D>::SharedPtr sub_vehicle;
 };
--- ROS2/day3/hw2/day3_hw2_package/src/day3_hw2_sub_node.cpp
+++ ROS2/day3/hw2/day3_hw2_package/src/day3_hw2_sub_node.cpp
@@ -3,12 +3,21 @@
 Day3Hw2SubNode::Day3Hw2SubNode() : Node("day3_hw2_sub_node")
 {
     sub_traffic = this->create_subscription<std_msgs::msg::String>(
-        "topic_traffic", 10, std::bind(&Day3Hw2SubNode::topic_callback, this, std::placeholders::_1));
+        "topic_traffic", 10, std::bind(&Day3Hw2SubNode::traffic_topic_callback, this, std::placeholders::_1));
+
+    sub_vehicle = this->create_subscription<geometry_msgs::msg::Pose2D>(
+        "topic_vehicle", 10, std::bind(&Day3Hw2SubNode::vehicle_topic_callback, this, std::placeholders::_1));
 }
 
-void Day3Hw2SubNode::topic_callback(const std_msgs::msg::String::SharedPtr msg)
+void Day3Hw2SubNode::traffic_topic_callback(const std_msgs::msg::String::SharedPtr msg)
 {
     traffic_state_ = msg->data;
+}
+
+void Day3Hw2SubNode::vehicle_topic_callback(const geometry_msgs::msg::Pose2D msg)
+{
+    cur_coordinate_.x = msg.x;
+    cur_coordinate_.y = msg.y;
 }
 
 int main(int argc, char *argv[])
