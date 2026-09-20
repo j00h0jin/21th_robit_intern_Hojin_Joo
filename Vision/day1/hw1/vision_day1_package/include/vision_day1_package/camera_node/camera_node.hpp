f8ac8ec343f3c38408f021a2cd610e958389caaf
j00h0jin <hojin7710@daum.net>
2026-09-21 03:04:47 +0900

    ✅ feat: add image pub

--- Vision/day1/hw1/vision_day1_package/include/vision_day1_package/camera_node/camera_node.hpp
+++ Vision/day1/hw1/vision_day1_package/include/vision_day1_package/camera_node/camera_node.hpp
@@ -1,11 +1,19 @@
 #pragma once
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/image.hpp"
 #include <opencv2/opencv.hpp>
 
 class CameraNode : public rclcpp::Node
 {
   public:
     CameraNode();
+    ~CameraNode();
 
   private:
+    void timerCallback();
+
+    rclcpp::Publisher<sensor_msgs::msg::Image>::SharedPtr image_pub;
+    rclcpp::TimerBase::SharedPtr timer;
+
+    cv::VideoCapture capture;
 };
--- Vision/day1/hw1/vision_day1_package/src/camera_node/camera_node.cpp
+++ Vision/day1/hw1/vision_day1_package/src/camera_node/camera_node.cpp
@@ -1,23 +1,54 @@
 #include "../include/vision_day1_package/camera_node/camera_node.hpp"
+#include "cv_bridge/cv_bridge.hpp"
 #include <iostream>
 
 CameraNode::CameraNode() : Node("camera_node")
 {
+    capture.open(0);
+
+    image_pub = this->create_publisher<sensor_msgs::msg::Image>("Image", 10);
+
+    timer = this->create_wall_timer(std::chrono::milliseconds(16), std::bind(&CameraNode::timerCallback, this));
 }
 
-int main()
+CameraNode::~CameraNode()
 {
-    cv::VideoCapture cap(0);
+    if (capture.isOpened())
+        capture.release();
+}
+
+void CameraNode::timerCallback()
+{
     cv::Mat frame;
-    while (true)
-    {
-        cap >> frame;
-        if (frame.empty())
-            break;
-        cv::imshow("camera", frame);
-        if (cv::waitKey(1) == 27)
-            break;
-    }
-    cap.release();
+    capture >> frame;
+
+    std_msgs::msg::Header header;
+    header.stamp = this->now();
+    header.frame_id = "camera_frame";
+
+    auto msg = cv_bridge::CvImage(header, "bgr8", frame).toImageMsg();
+
+    image_pub->publish(*msg);
+}
+
+int main(int argc, char *argv[])
+{
+    // cv::VideoCapture cap(0);
+    // cv::Mat frame;
+    // while (true)
+    // {
+    //     cap >> frame;
+    //     if (frame.empty())
+    //         break;
+    //     cv::imshow("camera", frame);
+    //     if (cv::waitKey(1) == 27)
+    //         break;
+    // }
+    // cap.release();
+    // return 0;
+
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<CameraNode>());
+    rclcpp::shutdown();
     return 0;
 }
