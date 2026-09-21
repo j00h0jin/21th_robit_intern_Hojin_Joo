f75afda56bcd87fb2621b51ccd07be9201172cd7
j00h0jin <hojin7710@daum.net>
2026-09-22 00:38:37 +0900

    ✅ faet: recognize yellow object

--- Vision/day2/hw1/vision_day2_hw1_package/include/vision_day2_hw1_package/hw1_node.hpp
+++ Vision/day2/hw1/vision_day2_hw1_package/include/vision_day2_hw1_package/hw1_node.hpp
@@ -1,12 +1,20 @@
 #pragma once
 
+#include "cv_bridge/cv_bridge.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include <opencv2/opencv.hpp>
+#include <sensor_msgs/msg/compressed_image.hpp>
+#include <sensor_msgs/msg/image.hpp>
+
+using img = sensor_msgs::msg::Image;
 
 class Hw1Node : public rclcpp::Node
 {
   public:
     Hw1Node();
+    ~Hw1Node();
 
   private:
+    rclcpp::Subscription<img>::SharedPtr image_sub;
+    void imageCallback(const img::SharedPtr msg);
 };
--- Vision/day2/hw1/vision_day2_hw1_package/src/hw1_node.cpp
+++ Vision/day2/hw1/vision_day2_hw1_package/src/hw1_node.cpp
@@ -6,13 +6,21 @@
 
 Hw1Node::Hw1Node() : Node("hw1_node")
 {
+    image_sub = this->create_subscription<img>("/camera1/camera/compressed_image", 10,
+                                               std::bind(&Hw1Node::imageCallback, this, std::placeholders::_1));
 }
 
-int main()
+Hw1Node::~Hw1Node()
 {
-    std::string package_path = ament_index_cpp::get_package_share_directory("vision_day1_hw2_package");
-    std::string img_path = package_path + "/resources/images/hw2.png";
-    cv::Mat image = cv::imread(img_path);
+    cv::destroyAllWindows();
+}
+
+void Hw1Node::imageCallback(const img::SharedPtr msg)
+{
+    cv_bridge::CvImagePtr cv_ptr;
+    cv_ptr = cv_bridge::toCvCopy(msg, "bgr8");
+
+    cv::Mat image = cv_ptr->image;
 
     if (image.empty())
         std::cout << "경로 확인";
@@ -22,65 +30,38 @@ int main()
 
     cv::Mat hsv;
     cv::cvtColor(image, hsv, cv::COLOR_BGR2HSV);
-    // H 0 ~ 179, S 0 ~ 255, V 0 ~ 255, H: 빨간색 끝에 걸쳐있음
+    // H 0 ~ 179, S 0 ~ 255, V 0 ~ 255
     // H 범위로 목표 색상을 선택, S 하한으로 저채도 영역을 제외, V 하한으로 저명도 영역을 제외
 
-    cv::Mat mask1_red, mask2_red, mask_red;
-    cv::inRange(hsv, cv::Scalar(0, 100, 100), cv::Scalar(9, 255, 255), mask1_red);
-    cv::inRange(hsv, cv::Scalar(170, 100, 100), cv::Scalar(179, 255, 255), mask2_red);
-    cv::bitwise_or(mask1_red, mask2_red, mask_red);
-    cv::Mat mask_green, mask_blue;
-    cv::inRange(hsv, cv::Scalar(33, 60, 60), cv::Scalar(85, 255, 255), mask_green);
-    cv::inRange(hsv, cv::Scalar(95, 90, 90), cv::Scalar(125, 255, 255), mask_blue);
+    cv::Mat mask_yellow;
+    cv::inRange(hsv, cv::Scalar(20, 100, 100), cv::Scalar(35, 255, 255), mask_yellow);
 
     // Clsoing
     cv::Mat kernel = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
-    cv::morphologyEx(mask_red, mask_red, cv::MORPH_CLOSE, kernel);
-    cv::morphologyEx(mask_green, mask_green, cv::MORPH_CLOSE, kernel);
-    cv::morphologyEx(mask_blue, mask_blue, cv::MORPH_CLOSE, kernel);
+    cv::morphologyEx(mask_yellow, mask_yellow, cv::MORPH_CLOSE, kernel);
 
     std::vector<std::vector<cv::Point>> contours;
-    cv::findContours(mask_red, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
+    cv::findContours(mask_yellow, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
     for (const auto &contour : contours)
     {
         double area = cv::contourArea(contour);
-        if (area > 100) // 작은 점 인식 방지
+        if (area > 30) // 작은 점 인식 방지
         {
             cv::Rect box = cv::boundingRect(contour);
-            cv::rectangle(image, box, cv::Scalar(0, 0, 255), 5);
-            break;
-        }
-    }
-    // cv::Rect box = cv::boundingRect(contours[0]);
-    // cv::rectangle(image, box, cv::Scalar(255, 0, 0), 5);
-
-    cv::findContours(mask_green, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    for (const auto &contour : contours)
-    {
-        double area = cv::contourArea(contour);
-        if (area > 100)
-        {
-            cv::Rect box2 = cv::boundingRect(contour);
-            cv::rectangle(image, box2, cv::Scalar(0, 255, 0), 5);
-            break;
-        }
-    }
-
-    cv::findContours(mask_blue, contours, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
-    for (const auto &contour : contours)
-    {
-        double area = cv::contourArea(contour);
-        if (area > 100)
-        {
-            cv::Rect box3 = cv::boundingRect(contour);
-            cv::rectangle(image, box3, cv::Scalar(255, 0, 0), 5);
+            cv::rectangle(image, box, cv::Scalar(0, 255, 255), 5);
             break;
         }
     }
 
     cv::imshow("image", image);
-    cv::waitKey(0);
-    cv::destroyAllWindows();
+    cv::waitKey(1); // 동영상
+}
 
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<Hw1Node>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
     return 0;
 }
