f4521c642d4be458c3b2bf02047e8c693692d0fa
j00h0jin <hojin7710@daum.net>
2026-09-23 19:45:12 +0900

    🌱 chore: add seed file

---
+++ Vision/day4/hw1/vision_day4_hw1_package/launch/launch.py
@@ -0,0 +1,15 @@
+from launch import LaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+
+
+def generate_launch_description():
+
+    return LaunchDescription([
+        Node(
+            package='vision_day4_hw1_package',
+            executable='',
+            name='',
+        )
+    ])
