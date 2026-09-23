a24b698ace08228d9c562f3f8399b326b78dee80
j00h0jin <hojin7710@daum.net>
2026-09-23 20:42:36 +0900

    🌱 chore: add seed file

--- Vision/day4/hw1/vision_day4_hw1_py_package/setup.py
+++ Vision/day4/hw1/vision_day4_hw1_py_package/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = 'vision_day4_hw1_py_package'
@@ -10,6 +13,8 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'models'), glob('models/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
